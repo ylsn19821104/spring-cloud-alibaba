b59719879ea01bf45a03c0cf0ae7d46d864f2d1f
ylsn19821104 <ylsn1982@126.com>
2019-01-15 11:02:19 +0800

    本地分支

--- spring-cloud-alibaba-examples/rocketmq-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/RocketMQApplication.java
+++ spring-cloud-alibaba-examples/rocketmq-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/RocketMQApplication.java
@@ -17,83 +17,81 @@ import org.springframework.messaging.SubscribableChannel;
  * @author <a href="mailto:fangjian0423@gmail.com">Jim</a>
  */
 @SpringBootApplication
-@EnableBinding({ MySource.class, MySink.class })
+@EnableBinding({MySource.class, MySink.class})
 public class RocketMQApplication {
 
-	public interface MySink {
+    public interface MySink {
 
-		@Input("input1")
-		SubscribableChannel input1();
+        @Input("input1")
+        SubscribableChannel input1();
 
-		@Input("input2")
-		SubscribableChannel input2();
+        @Input("input2")
+        SubscribableChannel input2();
 
-		@Input("input3")
-		SubscribableChannel input3();
+        @Input("input3")
+        SubscribableChannel input3();
 
-		@Input("input4")
-		SubscribableChannel input4();
-	}
+        @Input("input4")
+        SubscribableChannel input4();
+    }
 
-	public interface MySource {
-		@Output("output1")
-		MessageChannel output1();
+    public interface MySource {
+        @Output("output1")
+        MessageChannel output1();
 
-		@Output("output2")
-		MessageChannel output2();
-	}
+        @Output("output2")
+        MessageChannel output2();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(RocketMQApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQApplication.class, args);
+    }
 
-	@Bean
-	public CustomRunner customRunner() {
-		return new CustomRunner();
-	}
+    @Bean
+    public CustomRunner customRunner() {
+        return new CustomRunner();
+    }
 
-	@Bean
-	public CustomRunnerWithTransactional customRunnerWithTransactional() {
-		return new CustomRunnerWithTransactional();
-	}
+    @Bean
+    public CustomRunnerWithTransactional customRunnerWithTransactional() {
+        return new CustomRunnerWithTransactional();
+    }
 
-	public static class CustomRunner implements CommandLineRunner {
-		@Autowired
-		private SenderService senderService;
+    public static class CustomRunner implements CommandLineRunner {
+        @Autowired
+        private SenderService senderService;
 
-		@Override
-		public void run(String... args) throws Exception {
-			int count = 5;
-			for (int index = 1; index <= count; index++) {
-				String msgContent = "msg-" + index;
-				if (index % 3 == 0) {
-					senderService.send(msgContent);
-				}
-				else if (index % 3 == 1) {
-					senderService.sendWithTags(msgContent, "tagStr");
-				}
-				else {
-					senderService.sendObject(new Foo(index, "foo"), "tagObj");
-				}
-			}
-		}
-	}
+        @Override
+        public void run(String... args) throws Exception {
+            int count = 5;
+            for (int index = 1; index <= count; index++) {
+                String msgContent = "msg-" + index;
+                if (index % 3 == 0) {
+                    senderService.send(msgContent);
+                } else if (index % 3 == 1) {
+                    senderService.sendWithTags(msgContent, "tagStr");
+                } else {
+                    senderService.sendObject(new Foo(index, "foo"), "tagObj");
+                }
+            }
+        }
+    }
 
-	public static class CustomRunnerWithTransactional implements CommandLineRunner {
-		@Autowired
-		private SenderService senderService;
+    public static class CustomRunnerWithTransactional implements CommandLineRunner {
+        @Autowired
+        private SenderService senderService;
 
-		@Override
-		public void run(String... args) throws Exception {
-			// COMMIT_MESSAGE message
-			senderService.sendTransactionalMsg("transactional-msg1", false);
-			// ROLLBACK_MESSAGE message
-			senderService.sendTransactionalMsg("transactional-msg2", true);
-            // ROLLBACK_MESSAGE message
-			senderService.sendTransactionalMsg("transactional-msg3", true);
+        @Override
+        public void run(String... args) throws Exception {
             // COMMIT_MESSAGE message
-			senderService.sendTransactionalMsg("transactional-msg4", false);
-		}
-	}
+            senderService.sendTransactionalMsg("transactional-msg1", false);
+            // ROLLBACK_MESSAGE message
+            senderService.sendTransactionalMsg("transactional-msg2", true);
+            // ROLLBACK_MESSAGE message
+            senderService.sendTransactionalMsg("transactional-msg3", true);
+            // COMMIT_MESSAGE message
+            senderService.sendTransactionalMsg("transactional-msg4", false);
+        }
+    }
 
 }
